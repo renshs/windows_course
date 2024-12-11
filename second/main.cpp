d47c08a1462a993a000b97942c9259870df02bc7
Garaev Kamil <garaeff.camil@yandex.ru>
2024-12-11 20:59:58 +0300

    second task done in correct way

--- second/main.cpp
+++ second/main.cpp
@@ -1,75 +1,92 @@
-#include <iostream>
-#include <windows.h>
-#include <vector>
-#include <string>
+#include "windows.h"
+#include "stdio.h"
 
-void PrintPEInfo(const std::string& file_path) {
-    HANDLE file = CreateFileA(file_path.c_str(), GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
-    if (file == INVALID_HANDLE_VALUE) {
-        std::cerr << "Error: Failed to open the file." << std::endl;
-        return;
+BOOL LoadPeFile(LPCWSTR FilePath, PUCHAR* ppImageBase)
+{
+    HANDLE hFile = CreateFile(FilePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (INVALID_HANDLE_VALUE == hFile) {
+        printf("ERROR: LoadPeFile: CreateFile fails with %d error \n", GetLastError());
+        return false;
     }
 
-    HANDLE file_mapping = CreateFileMappingA(file, nullptr, PAGE_READONLY, 0, 0, nullptr);
-    if (!file_mapping) {
-        std::cerr << "Error: Failed to create file mapping." << std::endl;
-        CloseHandle(file);
-        return;
+    HANDLE hFileMapping = CreateFileMapping(hFile, NULL, PAGE_READONLY | SEC_IMAGE_NO_EXECUTE, 0, 0, NULL);
+    if (NULL == hFileMapping) {
+        printf("ERROR: LoadPeFile: CreateFileMapping fails with %d error \n", GetLastError());
+        CloseHandle(hFile);
+        return false;
     }
 
-    LPVOID file_base = MapViewOfFile(file_mapping, FILE_MAP_READ, 0, 0, 0);
-    if (!file_base) {
-        std::cerr << "Error: Failed to map file to memory." << std::endl;
-        CloseHandle(file_mapping);
-        CloseHandle(file);
-        return;
+    LPVOID p = MapViewOfFile(hFileMapping, FILE_MAP_READ, 0, 0, 0);
+    if (NULL == p) {
+        printf("ERROR: LoadPeFile: MapViewOfFile fails with %d error \n", GetLastError());
+        CloseHandle(hFileMapping);
+        CloseHandle(hFile);
+        return false;
     }
 
-    PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)file_base;
-    if (dos_header->e_magic != IMAGE_DOS_SIGNATURE) {
-        std::cerr << "Error: Not a PE file." << std::endl;
-        UnmapViewOfFile(file_base);
-        CloseHandle(file_mapping);
-        CloseHandle(file);
-        return;
-    }
-
-    PIMAGE_NT_HEADERS nt_headers = (PIMAGE_NT_HEADERS)((BYTE*)file_base + dos_header->e_lfanew);
-    if (nt_headers->Signature != IMAGE_NT_SIGNATURE) {
-        std::cerr << "Error: Missing NT header." << std::endl;
-        UnmapViewOfFile(file_base);
-        CloseHandle(file_mapping);
-        CloseHandle(file);
-        return;
-    }
-
-    WORD machine = nt_headers->FileHeader.Machine;
-    if (machine == IMAGE_FILE_MACHINE_I386) {
-        std::cout << "Architecture: x86" << std::endl;
-    } else if (machine == IMAGE_FILE_MACHINE_AMD64) {
-        std::cout << "Architecture: x64" << std::endl;
-    } else {
-        std::cout << "Architecture: Unknown" << std::endl;
-    }
-
-    std::cout << "Sections:" << std::endl;
-    PIMAGE_SECTION_HEADER section_header = IMAGE_FIRST_SECTION(nt_headers);
-    for (int i = 0; i < nt_headers->FileHeader.NumberOfSections; ++i) {
-        std::cout << "  " << std::string((char*)section_header->Name, strnlen((char*)section_header->Name, IMAGE_SIZEOF_SHORT_NAME)) << std::endl;
-        ++section_header;
-    }
-
-    UnmapViewOfFile(file_base);
-    CloseHandle(file_mapping);
-    CloseHandle(file);
+    *ppImageBase = (PUCHAR)p;
+    CloseHandle(hFileMapping);
+    CloseHandle(hFile);
+    return true;
 }
 
-int main(int argc, char* argv[]) {
+#define TO_PSTRUCT(TYPE, offset) (TYPE)(pImageBase + (offset)) // Convert RVA to structure pointer
+#define VAR_OF_PSTRUCT(var, TYPE, offset) TYPE var = TO_PSTRUCT(TYPE, offset)
+
+int wmain(int argc, wchar_t* argv[])
+{
     if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <path to PE file>" << std::endl;
-        return 1;
+        printf("Usage: PEAnalyzer <PeFilePath>\n");
+        return -1;
     }
 
-    PrintPEInfo(argv[1]);
+    LPCWSTR g_FilePath = argv[1];
+    PUCHAR pImageBase = nullptr;
+    if (!LoadPeFile(g_FilePath, &pImageBase)) return -1;
+
+    printf("MS-DOS Signature: %c%c \n", pImageBase[0], pImageBase[1]);
+    if (pImageBase[0] != 'M' || pImageBase[1] != 'Z') {
+        printf("ERROR: Not a valid PE file\n");
+        return -1;
+    }
+
+    PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)pImageBase;
+    VAR_OF_PSTRUCT(pTempPeHeader, PIMAGE_NT_HEADERS, pDosHeader->e_lfanew); // Offset to PE Header
+
+    PUCHAR p = (PUCHAR)(&pTempPeHeader->Signature);
+    if (p[0] != 'P' || p[1] != 'E') {
+        printf("ERROR: Invalid PE signature\n");
+        return -1;
+    }
+
+    printf("PE Signature: %c%c %x%x \n", p[0], p[1], p[2], p[3]);
+
+    WORD nSections = pTempPeHeader->FileHeader.NumberOfSections;
+    printf("PE Sections total: %d\n", nSections);
+
+    PIMAGE_SECTION_HEADER pSectionHeader = nullptr;
+    switch (pTempPeHeader->FileHeader.Machine) {
+        case IMAGE_FILE_MACHINE_I386:
+            printf("PE Architecture: x86\n");
+            pSectionHeader = (PIMAGE_SECTION_HEADER)(((PUCHAR)pTempPeHeader) + sizeof(IMAGE_NT_HEADERS32));
+            break;
+        case IMAGE_FILE_MACHINE_AMD64:
+            printf("PE Architecture: x64\n");
+            pSectionHeader = (PIMAGE_SECTION_HEADER)(((PUCHAR)pTempPeHeader) + sizeof(IMAGE_NT_HEADERS64));
+            break;
+        default:
+            printf("PE Architecture: unknown\n");
+            return -1;
+    }
+
+    CHAR nmSection[9];
+    memset(nmSection, 0, sizeof(nmSection));
+    for (int i = 0; i < nSections; i++) {
+        memcpy(nmSection, pSectionHeader->Name, 8);
+        printf("Section #%d: %s\n", i + 1, nmSection);
+        pSectionHeader++;
+    }
+
+    UnmapViewOfFile(pImageBase);
     return 0;
 }
